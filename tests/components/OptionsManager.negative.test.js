c2b4046fbc1c5465e1ca88a05179a12ef83c6955
Daniel Eagle <daniel@danieleagle.com>
2020-10-08 16:34:36 -0500

    [TEST] Added Unit Tests for OptionsManager

---
+++ tests/components/OptionsManager.negative.test.js
@@ -0,0 +1,150 @@
+// imports
+const AdmZip = require("adm-zip");
+const fileSystem = require("fs-extra");
+const shell = require("shelljs");
+const path = require("path");
+const OptionsManager = require("../../components/OptionsManager");
+const InvalidGitDataException = require("../../exceptions/InvalidGitDataException");
+const InvalidOptionException = require("../../exceptions/InvalidOptionException");
+const IllegalArgumentException = require("../../exceptions/IllegalArgumentException");
+const VersionFormattingException = require("../../exceptions/VersionFormattingException");
+
+describe("Tests the OptionsManager for proper functionality.", () => {
+  test("Tests applying the logging level target with an invalid argument.", () => {
+    expect(() => {
+      new OptionsManager().applyLoggingLevelTarget(10);
+    }).toThrow(IllegalArgumentException);
+
+    expect(() => {
+      new OptionsManager().applyLoggingLevelTarget("invalid");
+    }).toThrow(InvalidOptionException);
+    expect(() => {
+      new OptionsManager().applyLoggingLevelTarget(" ");
+    }).toThrow(InvalidOptionException);
+    expect(() => {
+      new OptionsManager().applyLoggingLevelTarget("Invalid Level");
+    }).toThrow(InvalidOptionException);
+  });
+
+  test("Tests validating the prod branch with an invalid argument.", () => {
+    expect(() => {
+      new OptionsManager().validateProdBranchName(10);
+    }).toThrow(IllegalArgumentException);
+    expect(() => {
+      new OptionsManager().validateProdBranchName(" ");
+    }).toThrow(InvalidGitDataException);
+    expect(() => {
+      new OptionsManager().validateProdBranchName("Invalid Branch");
+    }).toThrow(InvalidGitDataException);
+    expect(() => {
+      new OptionsManager().validateProdBranchName("Invalid Branch$#$@$)(@*!");
+    }).toThrow(InvalidGitDataException);
+    expect(() => {
+      new OptionsManager().validateProdBranchName("Way Too Long Way Too Long " +
+        "Way Too Long Way Too Long Way Too Long Way Too Long Way Too Long Way Too Long Way Too Long");
+    }).toThrow(InvalidGitDataException);
+  });
+
+  test("Tests validating the strategy type with an invalid argument.", () => {
+    expect(() => {
+      new OptionsManager().validateStrategyType(10);
+    }).toThrow(IllegalArgumentException);
+    expect(() => {
+      new OptionsManager().validateStrategyType(" ");
+    }).toThrow(InvalidOptionException);
+    expect(() => {
+      new OptionsManager().validateStrategyType("Invalid Strategy");
+    }).toThrow(InvalidOptionException);
+    expect(() => {
+      new OptionsManager().validateStrategyType("invalid");
+    }).toThrow(InvalidOptionException);
+  });
+
+  test("Tests validating the dev version appendage type with an invalid argument.", () => {
+    expect(() => {
+      new OptionsManager().validateDevVersionAppendageType(10);
+    }).toThrow(IllegalArgumentException);
+    expect(() => {
+      new OptionsManager().validateDevVersionAppendageType(" ");
+    }).toThrow(InvalidOptionException);
+    expect(() => {
+      new OptionsManager().validateDevVersionAppendageType("Invalid Appendage");
+    }).toThrow(InvalidOptionException);
+    expect(() => {
+      new OptionsManager().validateDevVersionAppendageType("invalid");
+    }).toThrow(InvalidOptionException);
+  });
+
+  test("Tests validating the change type with an invalid argument.", () => {
+    expect(() => {
+      new OptionsManager().validateChangeType(10);
+    }).toThrow(IllegalArgumentException);
+    expect(() => {
+      new OptionsManager().validateChangeType(" ");
+    }).toThrow(InvalidOptionException);
+    expect(() => {
+      new OptionsManager().validateChangeType("Invalid Change");
+    }).toThrow(InvalidOptionException);
+    expect(() => {
+      new OptionsManager().validateChangeType("invalid");
+    }).toThrow(InvalidOptionException);
+  });
+
+  test("Tests validating the proposed version with an invalid argument.", () => {
+    let testRepoZip = new AdmZip(path.join(__dirname, "/assets/test-repo.zip"));
+    let randomNumber = Math.floor((Math.random() * 10000) + 1);
+    testRepoZip.extractAllTo(path.join("/tmp", "/temp-" + parseInt(randomNumber, 10)), false);
+    shell.cd(path.join("/tmp", "/temp-" + parseInt(randomNumber, 10)));
+    shell.exec("git checkout master", { silent: false });
+
+    expect(() => {
+      new OptionsManager().validateProposedVersion(10);
+    }).toThrow(IllegalArgumentException);
+
+    expect(() => {
+      new OptionsManager().validateProposedVersion("test");
+    }).toThrow(IllegalArgumentException);
+
+    expect(() => {
+      new OptionsManager()
+          .validateProposedVersion({ strategy: "sequential", devAppendage: "snapshot", newVersion: "1.1.1.1" });
+    }).toThrow(IllegalArgumentException);
+
+    expect(() => {
+      new OptionsManager()
+          .validateProposedVersion({ strategy: "sequential", devAppendage: "snapshot", newVersion: " " });
+    }).toThrow(VersionFormattingException);
+
+    expect(() => {
+      new OptionsManager()
+          .validateProposedVersion({ strategy: "sequential", devAppendage: "snapshot", newVersion: "1 1" });
+    }).toThrow(VersionFormattingException);
+
+    shell.cd("..");
+    fileSystem.removeSync(path.join("/tmp", "/temp-" + parseInt(randomNumber, 10)));
+  });
+
+  test("Tests validating the commit messages with an invalid argument.", () => {
+    expect(() => {
+      new OptionsManager().validateCommitMessages(10, 20);
+    }).toThrow(IllegalArgumentException);
+    expect(() => {
+      new OptionsManager().validateCommitMessages(" ", " ");
+    }).toThrow(InvalidGitDataException);
+    expect(() => {
+      new OptionsManager().validateCommitMessages("This is a test", " ");
+    }).toThrow(InvalidGitDataException);
+  });
+
+  test("Tests validating the that the state of the branch will work for an initial commit with an invalid argument.", () => {
+    expect(() => {
+      new OptionsManager().validateInitialCommit();
+    }).toThrow(IllegalArgumentException);
+  });
+
+  test("Tests setting options using an invalid argument.", () => {
+    expect(() => {
+      new OptionsManager().set("test");
+    }).toThrow(IllegalArgumentException);
+  });
+});
---
+++ tests/components/OptionsManager.positive.test.js
@@ -0,0 +1,238 @@
+// imports
+const AdmZip = require("adm-zip");
+const fileSystem = require("fs-extra");
+const shell = require("shelljs");
+const path = require("path");
+const OptionsManager = require("../../components/OptionsManager");
+const VersioningAgent = require("../../components/VersioningAgent");
+const GitRunner = require("../../components/GitRunner");
+const Logger = require("../../components/Logger");
+const InvalidGitDataException = require("../../exceptions/InvalidGitDataException");
+const InvalidOptionException = require("../../exceptions/InvalidOptionException");
+const InvalidVersionException = require("../../exceptions/InvalidVersionException");
+
+describe("Tests the OptionsManager for proper functionality.", () => {
+  let testRepoZip = new AdmZip(path.join(__dirname, "/assets/test-repo.zip"));
+
+  test("Tests getting the settings folder name.", () => {
+    expect(OptionsManager.settingsFolderName).toBe(".ev");
+  });
+
+  test("Tests setting the logging level.", () => {
+    new OptionsManager().applyLoggingLevelTarget(Logger.Level.getName(Logger.Level.VERBOSE));
+    expect(Logger.currentLoggingLevel).toBe(Logger.Level.VERBOSE);
+  });
+
+  test("Tests validating the production branch name.", () => {
+    const validateProdBranchName = () => {
+      new OptionsManager().validateProdBranchName("master");
+    };
+
+    expect(validateProdBranchName).not.toThrow(InvalidGitDataException);
+  });
+
+  test("Tests validating the strategy type.", () => {
+    const validateStrategyType = () => {
+      new OptionsManager()
+          .validateStrategyType(VersioningAgent.StrategyType.getName(VersioningAgent.StrategyType.SEQUENTIAL));
+    };
+
+    expect(validateStrategyType).not.toThrow(InvalidOptionException);
+  });
+
+  test("Tests validating the dev version appendage type.", () => {
+    const validateDevVersionAppendageType = () => {
+      new OptionsManager().validateDevVersionAppendageType(
+          VersioningAgent.DevVersionAppendageType.getName(VersioningAgent.DevVersionAppendageType.SNAPSHOT)
+      );
+    };
+
+    expect(validateDevVersionAppendageType).not.toThrow(InvalidOptionException);
+  });
+
+  test("Tests validating the change type.", () => {
+    const validateChangeType = () => {
+      new OptionsManager().validateChangeType(
+          GitRunner.ChangeType.getName(GitRunner.ChangeType.VERSION_CHANGE)
+      );
+    };
+
+    expect(validateChangeType).not.toThrow(InvalidOptionException);
+  });
+
+  test("Tests validating the proposed version.", () => {
+    let randomNumber = Math.floor((Math.random() * 10000) + 1);
+    testRepoZip.extractAllTo(path.join("/tmp", "/temp-" + parseInt(randomNumber, 10)), false);
+    shell.cd(path.join("/tmp", "/temp-" + parseInt(randomNumber, 10)));
+    shell.exec("git checkout master", { silent: false });
+
+    let options = {};
+
+    const validateProposedVersion = () => {
+      new OptionsManager().validateProposedVersion(options);
+    };
+
+    options = {
+      newVersion: "3.0.2",
+      isInitialCommit: false,
+      strategy: "sequential",
+      devAppendage: "snapshot",
+      prodBranch: "master"
+    };
+
+    expect(validateProposedVersion).not.toThrow(InvalidVersionException);
+
+    options = {
+      newVersion: "1.0.2",
+      isInitialCommit: true,
+      strategy: "sequential",
+      devAppendage: "snapshot",
+      prodBranch: "master"
+    };
+
+    expect(validateProposedVersion).not.toThrow(InvalidVersionException);
+
+    shell.exec("git checkout latest", { silent: false });
+
+    expect(validateProposedVersion).not.toThrow(InvalidVersionException);
+
+    shell.cd("..");
+    fileSystem.removeSync(path.join("/tmp", "/temp-" + parseInt(randomNumber, 10)));
+  });
+
+  test("Tests validating commit messages.", () => {
+    let shortCommitMsg = "[BUG_FIX] Fixed a Horrible Bug";
+    let longCommitMsg = "This bug fix prevented the disruption of the space-time continuum.";
+
+    const validateCommitMessages = () => {
+      new OptionsManager().validateCommitMessages(shortCommitMsg, longCommitMsg);
+    };
+
+    expect(validateCommitMessages).not.toThrow(InvalidGitDataException);
+
+    shortCommitMsg = "This is a test. This is a test. This is a test. This is a test. This is a test. " +
+        "This is a test. This is a test. This is a test. This is a test. This is a test. This is a test.";
+
+    expect(validateCommitMessages).not.toThrow(InvalidGitDataException);
+
+    longCommitMsg = "This is a test. This is a test. This is a test. This is a test. This is a test. " +
+        "This is a test. This is a test. This is a test. This is a test. This is a test. This is a test." +
+        "This is a test. This is a test. This is a test. This is a test. This is a test. This is a test." +
+        "This is a test. This is a test. This is a test. This is a test. This is a test. This is a test." +
+        "This is a test. This is a test. This is a test. This is a test. This is a test. This is a test." +
+        "This is a test. This is a test. This is a test. This is a test. This is a test. This is a test." +
+        "This is a test. This is a test. This is a test. This is a test. This is a test. This is a test." +
+        "This is a test. This is a test. This is a test. This is a test. This is a test. This is a test." +
+        "This is a test. This is a test. This is a test. This is a test. This is a test. This is a test." +
+        "This is a test. This is a test. This is a test. This is a test. This is a test. This is a test.";
+
+    expect(validateCommitMessages).not.toThrow(InvalidGitDataException);
+  });
+
+  test("Tests validating the state of branch will work for an initial commit.", () => {
+    const validateInitialCommit = () => {
+      new OptionsManager().validateInitialCommit(new GitRunner(Logger.OutputType.SHELL).getCurrentBranchName());
+    };
+
+    let randomNumber = Math.floor((Math.random() * 10000) + 1);
+    shell.exec("mkdir " + path.join("/tmp", "/temp-" + parseInt(randomNumber, 10)), { silent: false });
+    shell.cd(path.join("/tmp", "/temp-" + parseInt(randomNumber, 10)));
+    shell.exec("git init", { silent: false });
+    shell.exec("git checkout -b master", { silent: false });
+
+    expect(validateInitialCommit).not.toThrow(InvalidGitDataException);
+
+    shell.cd("..");
+    fileSystem.removeSync(path.join("/tmp", "/temp-" + parseInt(randomNumber, 10)));
+  });
+
+  test("Tests setting options.", () => {
+    let randomNumber = Math.floor((Math.random() * 10000) + 1);
+    testRepoZip.extractAllTo(path.join("/tmp", "/temp-" + parseInt(randomNumber, 10)), false);
+    shell.cd(path.join("/tmp", "/temp-" + parseInt(randomNumber, 10)));
+    shell.exec("git checkout master", { silent: false });
+
+    let options = {
+      loggingLevel: "verbose",
+      prodBranch: "master",
+      strategy: "sequential",
+      devAppendage: "snapshot"
+    };
+
+    const setOptions = () => {
+      new OptionsManager().set(options);
+    };
+
+    expect(setOptions).not.toThrow(InvalidOptionException);
+
+    options = {
+      prodBranch: "master",
+      strategy: "sequential",
+      devAppendage: "snapshot"
+    };
+
+    expect(setOptions).not.toThrow(InvalidOptionException);
+
+    options = {
+      prodBranch: "master",
+      strategy: "sequential",
+      devAppendage: "snapshot",
+      manual: true,
+      isBreaking: true,
+      insertSkipCiTag: true
+    };
+
+    expect(setOptions).not.toThrow(InvalidOptionException);
+
+    options = {
+      prodBranch: "master",
+      devAppendage: "snapshot",
+      manual: true,
+      isBreaking: true,
+      insertSkipCiTag: true,
+      isInitialCommit: true
+    };
+
+    expect(setOptions).not.toThrow(InvalidOptionException);
+
+    options = {
+      devAppendage: "snapshot",
+      manual: true,
+      changeType: "bug_fix",
+      isBreaking: true,
+      insertSkipCiTag: true,
+      isInitialCommit: true,
+      shortMsg: "This is a test",
+      longMsg: "This is a test. This is a test. This is a test"
+    };
+
+    expect(setOptions).not.toThrow(InvalidOptionException);
+
+    options = {
+      changelogDirectory: "/tmp",
+      changelogFilename: "CHANGELOG.md"
+    };
+
+    expect(setOptions).not.toThrow(InvalidOptionException);
+
+    options = {
+      manual: true,
+      newVersion: "3.3.3",
+      changeType: "version_change"
+    };
+
+    expect(setOptions).not.toThrow(InvalidOptionException);
+
+    options = {
+      manual: true,
+      changeType: "bug_fix",
+      shortMsg: "This is a test",
+      longMsg: "This is a test. This is a test. This is a test"
+    };
+
+    expect(setOptions).not.toThrow(InvalidOptionException);
+
+    shell.cd("..");
+    fileSystem.removeSync(path.join("/tmp", "/temp-" + parseInt(randomNumber, 10)));
+  });
+});
